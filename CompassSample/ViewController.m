412330443a5460f40d309dd6780bd38b9f0b605f
Rex <rex45678@gmail.com>
2018-01-28 10:44:30 +0800

    刪除註解

--- CompassSample/ViewController.m
+++ CompassSample/ViewController.m
@@ -32,24 +32,6 @@
 -(void)locationManager:(nonnull CLLocationManager *)manager didUpdateHeading:(nonnull CLHeading *)newHeading
 {
     self.compassView.heading = newHeading;
-//    // 小於0是不正確的
-//    if (newHeading.headingAccuracy < 0) {
-//        return;
-//    }
-//    
-//    // 取得向北的角度
-//    CGFloat angle = newHeading.magneticHeading;
-//    
-//    NSLog(@"angle:%f",angle);
-//    
-//    // 轉探弧度
-//    CGFloat radian = angle / 180.0 * M_PI;
-//    
-//    NSLog(@"radian:%f",radian);
-//    // 反轉
-//    [UIView animateWithDuration:0.5 animations:^{
-//        self.compassView.compassPointerView.transform = CGAffineTransformMakeRotation(-radian);
-//    }];
 }
 
 @end
